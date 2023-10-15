66e00d3c2ec3c1f885268c3d5dfb935b41f908a6
Jazeb_Javed <jazebjaved52@gmail.com>
2023-10-16 03:11:40 +0500

    airbnb clone done

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
-
-import Card from './components/Card'
-import Hero from './components/Hero'
-import Navbar from './components/Navbar'
-import './css/App.css'
+import Card from "./components/Card";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import "./css/App.css";
+import data from "./data/data.js";
 
 
 export default function App() {
-  return (
-    <div className='app-container'>
-      <Navbar />
-      {/* <Hero /> */}
-      <Card 
-        image = 'image1.png'
-        rating = {5.0}
-        reviewsCount = {6}
-        country = 'USA'
-        title = 'Life lessons with Katie Zaferes'
-        price = {136}
-        status = 'sold out'
+  const cardsList = data.map((item) => {
+    console.log(item);
+    return (
+      <Card
+        item = {item}
       />
+    );
+  });
+
+  return (
+    <div className="app-container">
+      <Navbar />
+      <Hero />
+      <div className="cards-list-container">{cardsList}</div>
     </div>
-  )
+  );
 }
-
-
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,34 +5,48 @@ import dot from "../images/dot.svg";
 import image from "../images/image1.png";
 
 export default function Card(props) {
+
   console.log(props);
+
+  let badgeText = "";
+  if (props.item.openSpots === 0) {
+    badgeText = "sold out";
+  }
+  else if(props.item.location === "Online") {
+    badgeText = "online";
+  }
+  else {
+    badgeText = "";
+  }
+
+
   return (
     <>
       <div className="card-container">
         {/* Image */}
         <div className="card-image">
-          <img src= {`src/images/${props.image}`} />
-          <p className="badge">{props.status}</p>
+          <img src= {`src/images/${props.item.coverImg}`} />
+          {badgeText && <p className="badge">{badgeText}</p>}
         </div>
 
         {/* Reviews */}
         <div className="card-reviews">
           <img className="star" src={star} />
-          <span>{props.rating}</span>
-          <p>({props.reviewsCount})</p>
+          <span>{props.item.stats.rating}</span>
+          <p>({props.item.stats.reviewCount})</p>
           <img src={dot} />
-          <p>{props.country}</p>
+          <p>{props.item.location}</p>
         </div>
 
         {/* Title */}
         <div className="card-title">
-          <p>{props.title}</p>
+          <p>{props.item.title}</p>
         </div>
 
         {/* Rate */}
         <div className="card-rate">
           
-            <span>From ${props.price}</span> / person
+            <span>From ${props.item.price}</span> / person
          
         </div>
       </div>
