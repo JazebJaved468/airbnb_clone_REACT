3ad3e9290d80a50f742422de2d03482523223220
Jazeb_Javed <jazebjaved52@gmail.com>
2023-10-15 14:17:01 +0500

    architecture setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
     <App />
-  </React.StrictMode>,
-)
+  </>
+);
+
+// CMDs
+// cd new_app_14_oct
+// npm install
+// npm start
